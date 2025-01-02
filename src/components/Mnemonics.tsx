99c92ab79e25a5a2ec945ef5f10cf0a209c1b0ed
Aniket Dhakane <aniketdhakane3@gmail.com>
2025-01-02 23:51:54 +0530

    mnemonics component made

---
+++ src/components/Mnemonics.tsx
@@ -0,0 +1,23 @@
+interface MnemonicsProps {
+    mnemonic: string;
+}
+
+export default function Mnemonics( { mnemonic }: MnemonicsProps ) {    
+    const wordsArray = mnemonic.split(' ');
+    return(
+        <div className="w-[270px] md:w-[480px] p-8 bg-slate-800 rounded-xl">
+            <h2 className="text-rose-500 font-bold text-2xl">Your Secret Phrases:</h2>
+            <p className="text-rose-300 font-semibold text-sm">Please note that if you loose these, you loose your wallet.</p>
+            <div className="flex flex-wrap gap-2 pt-6 pb-4">
+                {wordsArray.map((word, index) => (
+                    <div 
+                        key={index} 
+                        className="bg-gray-700 text-white w-24 py-2 px-4 rounded-lg shadow-sm text-center"
+                    >
+                        {word}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+};
--- src/pages/onBoardingPage.tsx
+++ src/pages/onBoardingPage.tsx
@@ -1,5 +1,16 @@
+import Mnemonics from "../components/Mnemonics";
+import { generateMnemonic } from "bip39"
+
 export default function OnBoardingPage() {
+    const mnemonic = generateMnemonic(128);
     return(
-        <p>This is the onboarding page, here all the logic would be written.</p>
+        <>
+            <div className="w-screen h-screen flex flex-col justify-center items-center">
+            <Mnemonics mnemonic={mnemonic}/>
+            </div>
+            <div className="absolute -z-10 inset-0 h-full w-full 
+            bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] 
+            bg-[size:20px_20px]" />
+        </>
     )
 };
