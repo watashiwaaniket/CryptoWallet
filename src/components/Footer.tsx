06a678024c9e3048aceaadf3c05e77aa7b281031
Aniket Dhakane <aniketdhakane3@gmail.com>
2025-01-05 16:56:19 +0530

    added footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return(
+        <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-x-12 gap-y-3 px-6 py-4 text-center mt-4">
+        <p className="font-sans antialiased text-slate-700 text-base text-current">
+            © 2025 Slingpack
+        </p>
+        </footer>
+
+    )
+};
--- src/pages/onBoardingPage.tsx
+++ src/pages/onBoardingPage.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer";
 import Mnemonics from "../components/Mnemonics";
 import Wallet from "../components/Wallet";
 import { generateMnemonic } from "bip39"
@@ -11,7 +12,8 @@ export default function OnBoardingPage() {
             <div className="w-screen min-h-screen flex flex-col justify-center items-center overflow-y-auto mt-6">
             <Mnemonics mnemonic={mnemonic}/>
             <Wallet mnemonic={mnemonic}/>
-            </div>
+            <Footer />
+            </div>            
             <div className="absolute -z-10 inset-0 h-full w-full 
             bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] 
             bg-[size:20px_20px]" />
