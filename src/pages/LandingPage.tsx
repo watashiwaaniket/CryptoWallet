f69bef5b332077d13a42540a6ec6b6059020bfd8
Aniket Dhakane <aniketdhakane3@gmail.com>
2025-01-05 16:18:45 +0530

    updated the build

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,12 +5,12 @@ export default function LandingPage() {
     return(
         <div className="relative min-h-screen flex flex-col items-center justify-center">
         <div className='flex justify-center items-center min-h-screen'>    
-            <div className='w-[270px] md:w-[480px] p-8 bg-slate-800 rounded-xl'>
+            <div className='w-[360px] md:w-[480px] p-8 bg-slate-800 rounded-xl'>
                 <div className='flex flex-col justify-center items-center'>
-                    <h1 className='text-4xl font-bold text-rose-500 pt-6'>Welcome to Slingpack!</h1>
-                    <p className='text-sm text-white font-semibold pb-8'>Your personal crypto wallet on the go</p>
+                    <h1 className='text-2xl md:text-4xl font-bold text-rose-500 pt-6'>Welcome to Slingpack!</h1>
+                    <p className='text-xs md:text-sm text-white font-semibold pb-8'>Your personal crypto wallet on the go</p>
                     <img src="/slingpack.avif" alt="slingLogo" className='rounded-xl'/>
-                    <Link to='/home' className='bg-rose-500 px-6 py-2 rounded-2xl mt-8 text-xl font-bold text-slate-900'>Lets Go!</Link>
+                    <Link to='/home' className='bg-rose-500 px-6 py-2 rounded-2xl mt-8 text-lg md:text-xl font-bold text-slate-900'>Lets Go!</Link>
                 </div>
             </div>
         </div>
